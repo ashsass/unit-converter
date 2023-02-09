725e238144ee91502404b7ebd8be9c0b8546360c
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-02-09 11:09:52 -0500

    Completed basic conversion

--- index.js
+++ index.js
@@ -6,13 +6,27 @@
 
 const convertBtn = document.getElementById("convert-btn")
 const convertInput = document.getElementById("convert-input")
+const lengthConversion = document.getElementById("length")
+const volumeConversion = document.getElementById("volume")
+const massConversion = document.getElementById("mass")
 
 convertBtn.addEventListener("click", function() {
     convert(convertInput.value)
 })
 
 function convert(num) {
-    //Meter to feet
+    // meter/feet
     let feet = num * 3.281
-    console.log(`${num} meter(s) = ${feet} feet`)
+    let meter = num / 3.281
+    lengthConversion.textContent = `${num} meter(s) = ${feet} feet | ${num} feet = ${meter} meter(s)`
+
+    // liter/gallon
+    let liter = num / 0.264
+    let gallon = num * 0.264
+    volumeConversion.textContent = `${num} liter(s) = ${gallon} gallon(s) | ${num} gallon = ${liter} liter(s)`
+
+    // kg/lbs
+    let kgs = num * 2.204
+    let lbs = num / 2.204
+    massConversion.textContent = `${num} kg(s) = ${kgs} lb(s) | ${num} lb(s) = ${lbs} kg(s)`
 }
