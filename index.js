f4bd61d33d20c69e467a3658e2e8ffd49d903d5f
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-02-09 10:59:15 -0500

    Created basic convert function

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const convertBtn = document.getElementById("convert-btn")
+const convertInput = document.getElementById("convert-input")
+
+convertBtn.addEventListener("click", function() {
+    convert(convertInput.value)
+})
+
+function convert(num) {
+    //Meter to feet
+    let feet = num * 3.281
+    console.log(`${num} meter(s) = ${feet} feet`)
+}
