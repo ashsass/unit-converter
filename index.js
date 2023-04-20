ade66ef6057f0e96a37ecd3ed40db324c152dc1f
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-04-20 13:38:05 -0400

    Update README

--- index.js
+++ index.js
@@ -6,6 +6,9 @@
 
 const convertBtn = document.getElementById("convert-btn")
 const convertInput = document.getElementById("convert-input")
+const lengthConversion = 3.281
+const volumeConversion = 0.264
+const massConversion = 2.204
 
 convertBtn.addEventListener("click", function() {
     if(convertInput.value){
@@ -14,25 +17,25 @@ convertBtn.addEventListener("click", function() {
 })
 
 function convert(num) {
-    const lengthConversion = 3.281
-    const volumeConversion = 0.264
-    const massConversion = 2.204
-    
     // meter/feet
     let feet = (num * lengthConversion)
     let meter = (num / lengthConversion)
-    document.getElementById("length").textContent = `${num} meter(s) = ${feet} feet | ${num} feet = ${meter} meter(s)`
-
+    
     // liter/gallon
     let liter = (num / volumeConversion).toFixed(3)
     let gallon = (num * volumeConversion).toFixed(3)
-    document.getElementById("volume").textContent = `${num} liter(s) = ${gallon} gallon(s) | ${num} gallon(s) = ${liter} liter(s)`
-
+    
     // kg/lbs
     let kgs = (num * massConversion).toFixed(3)
     let lbs = (num / massConversion).toFixed(3)
-    document.getElementById("mass").textContent = `${num} kg(s) = ${kgs} lb(s) | ${num} lb(s) = ${lbs} kg(s)`
+    
 }
 
+function render(num) {
+    convert(num)
+    document.getElementById("length").textContent = `${num} meter(s) = ${feet} feet | ${num} feet = ${meter} meter(s)`
+    document.getElementById("volume").textContent = `${num} liter(s) = ${gallon} gallon(s) | ${num} gallon(s) = ${liter} liter(s)`
+    document.getElementById("mass").textContent = `${num} kg(s) = ${kgs} lb(s) | ${num} lb(s) = ${lbs} kg(s)`
+}
 
 //Refactor the decimal place code and have it so that if that last number is 0 omit it from displaying 
